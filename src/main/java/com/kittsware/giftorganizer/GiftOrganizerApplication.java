21967fb80901b2ea8e54bc8064c60f4c437cbd2c
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-06 12:47:13 -0600

    Initial commit

---
+++ src/main/java/com/kittsware/giftorganizer/GiftOrganizerApplication.java
@@ -0,0 +1,13 @@
+package com.kittsware.giftorganizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftOrganizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GiftOrganizerApplication.class, args);
+    }
+
+}
