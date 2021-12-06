f365eeda3e59a6f1a1f415572041d9632fbb35be
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-06 12:55:05 -0600

    Created User entity for MVP

---
+++ src/main/java/com/kittsware/giftorganizer/models/User.java
@@ -0,0 +1,40 @@
+package com.kittsware.giftorganizer.models;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String userName;
+    @Column(unique = true)
+    private String userEmail;
+
+    public User() {}
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
