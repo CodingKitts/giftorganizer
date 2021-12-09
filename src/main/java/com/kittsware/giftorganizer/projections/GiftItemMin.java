bf14bdd81d1ff1b2f891bb41dac13ff58948d7a9
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-09 11:31:28 -0600

    updated the projection to allow the ID to be passed, this will ensure we can update appropriately

--- src/main/java/com/kittsware/giftorganizer/projections/GiftItemMin.java
+++ src/main/java/com/kittsware/giftorganizer/projections/GiftItemMin.java
@@ -1,6 +1,7 @@
 package com.kittsware.giftorganizer.projections;
 
 public interface GiftItemMin {
+    Long getGiftItemId();
     String getGiftItemName();
     Double getGiftItemPrice();
     String getOwnerEmail();
