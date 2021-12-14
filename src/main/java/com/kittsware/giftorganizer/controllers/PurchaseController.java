f78f5d1708cb81ba9cb236ba41af9b338e17eed3
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-13 21:47:25 -0600

    Removing comments

--- src/main/java/com/kittsware/giftorganizer/controllers/PurchaseController.java
+++ src/main/java/com/kittsware/giftorganizer/controllers/PurchaseController.java
@@ -17,17 +17,6 @@ public class PurchaseController {
         this.purchaseService = purchaseService;
     }
 
-
-    //TODO: Okay so the problem is that you are trying to create a Purchase object at a later time than when the GiftItem
-    //      was created. However, you would need to retrieve the GiftItem object in the Purchase Service in order to
-    //      properly attach the two objects to each other.
-    //      I want to keep the purchase service stuff separate from the gift item. So in order to do this, we need to
-    //      create a purchase object when the gift item is created. Okay if i do this then I might as well keep purchase
-    //      info as part of the Gift Item class... Why did I want to separate the Purchase object?
-    //      What if I made them separate objects? I would still need to have a combined service with both repos.
-    //
-    //      Another option is to completely separate the purchase from the gift. Remove the connection and do a good
-    //      job coordinating the two objects..
     @PostMapping("/purchase")
     public Purchase purchaseItem(@RequestBody Purchase purchase) {
         return this.purchaseService.createPurchase(purchase);
