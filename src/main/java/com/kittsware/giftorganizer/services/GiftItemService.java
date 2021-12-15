b3976c45453acdef80da2b33cf1fdaeeabc75326
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-15 11:58:43 -0600

    Added organization and todos to Gift Item service

--- src/main/java/com/kittsware/giftorganizer/services/GiftItemService.java
+++ src/main/java/com/kittsware/giftorganizer/services/GiftItemService.java
@@ -7,17 +7,24 @@ import java.util.Collection;
 import java.util.List;
 
 public interface GiftItemService {
+    //TODO: Reminder - Make sure to send a Notification to Buyer when Owner deletes a Purchased Item
+    //TODO: What do I need my Service to be able to do?
+    /*
+        Create a new GiftItem
+        Get all Minimized GiftItems for Owner
+        Get all Full GiftItems for Friend
+        Get a single Minimized GiftItem for Owner
+        Get a single Full GiftItem for Friend
+        Update a GiftItem as Owner
+        Update a GiftItem as Friend (Purchase)
+        Update a GiftItem as Friend (Return)
+        Delete a GiftItem as Owner
+        Delete all GiftItems as Owner
+     */
+
     Collection<GiftItemMin> getAllItemsForOwner(String ownerEmail);
     GiftItem createGiftItem(GiftItem giftItem);
     List<GiftItem> getAllItems();
-    //What else do I need to be able to do with Wish Items?
-    /*
-        Get Items as a Friend
-        Update Item as an Owner
-        Update Item as a Friend (Purchase)
-        Delete an Item as an Owner
-
-     */
 
     boolean deleteAllGiftItems(String ownerEmail);
     boolean deleteItemById(Long itemId);
