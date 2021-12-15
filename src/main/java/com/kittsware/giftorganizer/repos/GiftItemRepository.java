ad53c157696563a5ae4c4f59833775ee70cbda66
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-15 11:51:31 -0600

    Cleaned up GiftItem Repo

--- src/main/java/com/kittsware/giftorganizer/repos/GiftItemRepository.java
+++ src/main/java/com/kittsware/giftorganizer/repos/GiftItemRepository.java
@@ -6,20 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
-import java.util.List;
 
 @Repository
 public interface GiftItemRepository extends JpaRepository<GiftItem, Long> {
-    //TODO: Create custom method to return a minimized version of the GiftItem, one with no bought info, return to Owner.
-
-    /*
-        Do I need to create a separate Entity class for Purchase Information?
-        If yes, I would abstract as much Purchase information from the Gift Item as possible. Things like Buyer Email,
-        purchase date, etc. This would make getting an Owner specific version of Gift Items easy, however to get a list
-        purchased items as well as the original list would require DB coordination. What if I tie a Purchase object to
-        the wish item? Like there is one purchase to one wish item. It would start out null?
-     */
-    //List<GiftItemMin> findGiftItemsByOwnerEmail(String ownerEmail);
     Collection<GiftItemMin> findGiftItemsByOwnerEmail(String ownerEmail);
 
     int deleteGiftItemsByOwnerEmail(String ownerEmail);
