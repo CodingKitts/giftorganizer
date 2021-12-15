423981d79e2fe13eb211eecaef44d7313017468d
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-15 12:01:42 -0600

    Cleaned up Controller comments and added basic todo

--- src/main/java/com/kittsware/giftorganizer/controllers/GiftItemController.java
+++ src/main/java/com/kittsware/giftorganizer/controllers/GiftItemController.java
@@ -20,6 +20,8 @@ public class GiftItemController {
         this.giftItemService = giftItemService;
     }
 
+    //TODO: Create API methods for the Service functions.
+
     @GetMapping("/items")
     public Collection<GiftItemMin> getItemsForOwner(Principal principal) {
         return this.giftItemService.getAllItemsForOwner(principal.getName());
@@ -30,25 +32,16 @@ public class GiftItemController {
         return this.giftItemService.getAllItems();
     }
 
-    //TODO: Create function to getItems for NonOwner
-    //TODO: Create function to edit Items for Owner
-
     @PostMapping("/item")
     public GiftItem createGiftItem(@RequestBody GiftItem giftItem) {
         return this.giftItemService.createGiftItem(giftItem);
     }
-
-    /*
-        Okay so what do I need to do now? The GiftItem will not return any information about the Purchase. Its also
-        difficult to attach a purchase to the wish item when the purchase gets created.
-     */
 
     @DeleteMapping("/items")
     public boolean deleteAllGiftItems(@RequestBody String ownerEmail) {
         return this.giftItemService.deleteAllGiftItems(ownerEmail);
     }
 
-    //TODO: If I delete an Item, does it delete the Purchase? (If one exists)
     @DeleteMapping("/item/{itemId}")
     public boolean deleteItemById(@PathVariable Long itemId) {
         return this.giftItemService.deleteItemById(itemId);
