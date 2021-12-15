4157ef0b0fab422746b27070673d836e70dbf33c
Harrison Kitts <harrisonkitts@Harrisons-MacBook-Pro.local>
2021-12-15 12:03:38 -0600

    Deleted all Purchase classes

--- src/main/java/com/kittsware/giftorganizer/repos/PurchaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kittsware.giftorganizer.repos;
-
-import com.kittsware.giftorganizer.entities.Purchase;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PurchaseRepository extends JpaRepository<Purchase, Long> {
-}
